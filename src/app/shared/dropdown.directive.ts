b47c242861bddd0270cda80d518fd305855d33c8
abolka <bolka.aleksander@gmail.com>
2018-04-27 19:46:31 +0200

    Migrate to bootstrap4

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,16 +1,33 @@
-import {Directive, HostBinding, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
 
-  @HostBinding('class.open') isOpen = false;
+  isOpen = false;
+
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) {
+  }
 
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
+
+    this.changeElementClass();
   }
 
-  constructor() { }
+  private changeElementClass() {
+    let div: HTMLDivElement;
+    for (let child of this.elementRef.nativeElement.children) {
+      if (child instanceof HTMLDivElement) {
+        div = child;
+      }
+    }
+    if (this.isOpen) {
+      this.renderer.addClass(div, 'show');
+    } else {
+      this.renderer.removeClass(div, 'show');
+    }
+  }
 
 }
